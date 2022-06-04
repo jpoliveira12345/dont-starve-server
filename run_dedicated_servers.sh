e7d83e09c5fa37f5286f9c326acc88b1b63a9ea7
João Paulo <joaopaulodeoliveira123@gmail.com>
2022-06-04 19:09:24 -0300

    Update run_dedicated_servers.sh

--- run_dedicated_servers.sh
+++ run_dedicated_servers.sh
@@ -4,15 +4,15 @@ cluster_name="Cluster_1"
 dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
 
 function fail(){
-	echo Error: "$@" >&2
-	exit 1
+        echo Error: "$@" >&2
+        exit 1
 }
 
 function check_for_file()
 {
-	if [ ! -e "$1" ]; then
-		fail "Missing file: $1"
-	fi
+        if [ ! -e "$1" ]; then
+                fail "Missing file: $1"
+        fi
 }
 
 
@@ -22,14 +22,13 @@ check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
 check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
-#steamcmd +force_install_dir "$install_dir" +login sickiller675 v7\(dNM6i~cH/Tye +app_update 343050 validate +quit
 steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 
 check_for_file "$install_dir/bin64"
 
-cd "$install_dir/bin" || fail
+cd "$install_dir/bin64" || fail
 
-run_shared=(./dontstarve_dedicated_server_nullrenderer)
+run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
 run_shared+=(-console)
 run_shared+=(-cluster "$cluster_name")
 run_shared+=(-monitor_parent_process $$)
