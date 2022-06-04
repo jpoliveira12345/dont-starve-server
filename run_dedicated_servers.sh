ce71b751b8602b5860ab0fc094d9a95011098997
João Paulo de Oliveira <joaopaulodeoliveira123@gmail.com>
2022-06-04 21:27:23 +0000

    added mandatory files to run the server

---
+++ run_dedicated_servers.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+install_dir="$HOME/steamapps/DST"
+cluster_name="Cluster_1"
+dontstarve_dir="$HOME/.klei/DoNotStarveTogether"
+
+function fail(){
+	echo Error: "$@" >&2
+	exit 1
+}
+
+function check_for_file()
+{
+	if [ ! -e "$1" ]; then
+		fail "Missing file: $1"
+	fi
+}
+
+
+
+check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
+check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
+check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
+check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
+
+#steamcmd +force_install_dir "$install_dir" +login sickiller675 v7\(dNM6i~cH/Tye +app_update 343050 validate +quit
+steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+
+check_for_file "$install_dir/bin64"
+
+cd "$install_dir/bin" || fail
+
+run_shared=(./dontstarve_dedicated_server_nullrenderer)
+run_shared+=(-console)
+run_shared+=(-cluster "$cluster_name")
+run_shared+=(-monitor_parent_process $$)
+
+"${run_shared[@]}" -shard Caves  | sed 's/^/Caves:  /' &
+"${run_shared[@]}" -shard Master | sed 's/^/Master: /'
+#${run_shared[@]} -shard Master
